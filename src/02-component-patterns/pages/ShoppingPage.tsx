f86673a2374a355071cc43b228049fd3e6e6aa5b
jfloresi <jfloresi@everis.com>
2022-05-30 18:04:25 +0200

    feat: adding shopping cart

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
 import ProductCard from "../components";
+import { ProductButtons } from "../components/ProductButtons";
+import { ProductImage } from "../components/ProductImage";
+import { ProductTitle } from "../components/ProductTitle";
+import { Product } from "../interfaces/interfaces";
 import "../styles/custom-styles.css";
 
 const product = {
@@ -7,16 +12,84 @@ const product = {
   img: "./coffee-mug.png",
 };
 
+const product2 = {
+  id: "2",
+  title: "Coffee Mug - Meme",
+  img: "./coffee-mug2.png",
+};
+
+const products: Product[] = [product, product2];
+
+interface ProductInCart extends Product {
+  count: number;
+}
+
 export const ShoppingPage = () => {
+  const [shoppingCart, setShoppingCart] = useState<{
+    [key: string]: ProductInCart;
+  }>({});
+
+  const onProductCountChange = ({
+    count,
+    product,
+  }: {
+    count: number;
+    product: Product;
+  }) => {
+    setShoppingCart((shoppingCart) => {
+      let updatedShoppingCart: {
+        [key: string]: ProductInCart;
+      } = { ...shoppingCart };
+
+      if (count > 0) {
+        updatedShoppingCart[product.id] = { ...product, count };
+      } else {
+        delete updatedShoppingCart[product.id];
+      }
+
+      return { ...updatedShoppingCart };
+    });
+  };
+
   return (
     <div>
       <h1>Shopping Store</h1>
       <hr />
-      <ProductCard product={product} className={"bg-dark"}>
-        <ProductCard.Image className={"custom-image"} />
-        <ProductCard.Title className={"text-white text-bold"} />
-        <ProductCard.Buttons className={"text-white custom-buttons"} />
-      </ProductCard>
+
+      <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            className="bg-dark text-white"
+            onChange={onProductCountChange}
+          >
+            <ProductImage
+              className="custom-image"
+              style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}
+            />
+            <ProductTitle className="text-bold" />
+            <ProductButtons className="custom-buttons" />
+          </ProductCard>
+        ))}
+      </div>
+      <div className="shopping-cart">
+        {Object.entries(shoppingCart).map((product) => (
+          <ProductCard
+            key={product[1].id}
+            product={product[1]}
+            className="bg-dark text-white"
+            onChange={onProductCountChange}
+          >
+            <ProductImage
+              className="custom-image"
+              style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}
+            />
+            <ProductTitle className="text-bold" />
+            <ProductButtons className="custom-buttons" />
+          </ProductCard>
+        ))}
+      </div>
     </div>
   );
 };
